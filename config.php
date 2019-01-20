18d594670fde4a62a8c7f675bd15659f06f1d52c
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-20 13:23:14 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php
 $config = [
 	"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx",
+	
+	
 //kamu bisa memasukan akun lainya dengan cara copy seperti di atas
-	//contohnya seperti ini "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx",
+//contohnya seperti ini "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx",
 ];
